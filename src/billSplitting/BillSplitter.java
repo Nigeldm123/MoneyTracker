444cf1599e5af0d2c51eb71fa1b7c3191022f180
Femke <femke.w@telenet.be>
2023-12-05 13:40:40 +0100

    update

--- src/billSplitting/BillSplitter.java
+++ src/billSplitting/BillSplitter.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.prefs.PreferenceChangeEvent;
 
 public class BillSplitter {
     private GlobalBill bill;
@@ -21,7 +22,7 @@ public class BillSplitter {
     public void payBill() {
         for (Map<PersonEntry, Map<TicketEntry.eventsEnum, Pair<Double, Set<PersonEntry>>>> equalBill : bill.getEqualBill()) {
             for (PersonEntry receiver : equalBill.keySet()) {
-                paymentPerPerson = new HashMap<>();                                                 // for every receiver new bill
+                paymentPerPerson = new HashMap<>();                                                 // for every payer new bill
                 for (TicketEntry.eventsEnum event : equalBill.get(receiver).keySet()) {
                     Pair<Double, Set<PersonEntry>> ticket = equalBill.get(receiver).get(event);
                     Integer amount_of_people = ticket.getValue1().size();
@@ -36,7 +37,39 @@ public class BillSplitter {
                         }
                     }
                 }
+                totalPayment.put(receiver,paymentPerPerson);
             }
         }
+
+        for (Map<PersonEntry, Map<TicketEntry.eventsEnum, Map<PersonEntry, Double>>> unequalBill : bill.getRegularBill()) {
+            for (PersonEntry receiver : unequalBill.keySet()) {
+                paymentPerPerson = new HashMap<>();                                                 // for every payer new bill
+                for (TicketEntry.eventsEnum event : unequalBill.get(receiver).keySet()) {
+                    Map<PersonEntry,Double> ticket = unequalBill.get(receiver).get(event);
+
+                    for (PersonEntry payer : ticket.keySet()) {
+                        if (paymentPerPerson.containsKey(payer)) {
+                            Double new_payment = paymentPerPerson.get(payer) + ticket.get(payer);      // add new payment to already existing payment
+                            paymentPerPerson.replace(payer, new_payment);
+                        } else {
+                            paymentPerPerson.put(payer, ticket.get(payer));
+                        }
+                    }
+                }
+
+                if (totalPayment.containsKey(receiver)) {
+                    Map<PersonEntry,Double> oldPaymentPerPerson = totalPayment.get(receiver);
+                    oldPaymentPerPerson.forEach(
+                            (key, value) -> paymentPerPerson.merge(key, value, Double::sum));       // merge two existing maps (old + new)
+                    totalPayment.replace(receiver,paymentPerPerson);
+                } else {
+                    totalPayment.put(receiver,paymentPerPerson);
+                }
+            }
+        }
+    }
+
+    public Map<PersonEntry, Map<PersonEntry, Double>> getTotalPayment() {
+        return totalPayment;
     }
 }
